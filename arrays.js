a69c777adfdc49179070d56c49025383b155781c
Vicente Soriano <vicente.soriano@icloud.com>
2018-07-23 18:10:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function accessElementInArray(array, index) {
   return(array[index])
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  
+  array.shift()
   return(array)
 }
 function removeElementFromBeginningOfArray(array) {
