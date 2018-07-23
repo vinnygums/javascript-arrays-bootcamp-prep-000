06eb2a84c9efaf2cd60fe81c81c72d3a9c25210e
Vicente Soriano <vicente.soriano@icloud.com>
2018-07-23 17:16:51 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = ['snickers', 'hudred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  return([element, ...array])
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  return(array[n] = n)
+}
+function accessElementInArray(array, index) {
+  return(array[index])
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return(array.shift())
+}
+function removeElementFromBeginningOfArray(array) {
+  return(array.slice(1))
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return(array.pop())
+}
+function removeElementFromEndOfArray(array) {
+  return(array.slice(0, array.length - 1))
+}
