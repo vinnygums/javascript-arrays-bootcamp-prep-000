168c8c60c2c38c52c6214f268beb8e66bf168e8d
Vicente Soriano <vicente.soriano@icloud.com>
2018-07-23 18:15:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array, element) {
   return([element, ...array])
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift()
+  array.unshift(element)
   return(array)
 }
 function addElementToEndOfArray(array, element) {
